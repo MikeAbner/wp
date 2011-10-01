8708021e942ea7d7db7306ab21716b2f01948e76
Mike Abner <mike.abner@gmail.com>
2011-09-30 22:02:22 -0700

    Add a logout link that only works half way and don't show controls to users who aren't the owner of the post on the show page!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -38,6 +38,7 @@ class SessionsController < ApplicationController
   
   def destroy
     session['user_id'] = nil
-    render :json => { :status => :ok }
+    #render :json => { :status => :ok }
+    redirect_to root_path
   end
 end
