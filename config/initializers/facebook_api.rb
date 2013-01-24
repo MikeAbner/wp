ae6b554c488ea0c3d6bdc3c26a3c0a6b736cb9bc
Mike Abner <mike.abner@gmail.com>
2013-01-24 13:37:59 -0800

    remove api keys

--- config/initializers/facebook_api.rb
+++ config/initializers/facebook_api.rb
@@ -1,9 +1,9 @@
 if Rails.env.development?
-  FB_APP_ID = "167725063305571"
-  FB_SECRET = "29179ba2836119847c619d980fcf056a"
+  FB_APP_ID = ""
+  FB_SECRET = ""
 elsif Rails.env.test?
-  FB_APP_ID = "167725063305571"
-  FB_SECRET = "29179ba2836119847c619d980fcf056a"
+  FB_APP_ID = ""
+  FB_SECRET = ""
 elsif Rails.env.production?
   FB_APP_ID = ENV['FB_APP_ID']
   FB_SECRET = ENV['FB_SECRET']
